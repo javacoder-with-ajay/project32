09aed5317a807eab552dde721246c65fa0372d57
javacoder-with-ajay <javacoder753@gmail.com>
2024-11-25 00:05:48 +0530

    Add files via upload

---
+++ ajay.java
@@ -0,0 +1,11 @@
+public class ajay
+{
+public static void main(String[] args) {
+    
+    System.out.println("my name is vijay kumar");
+    
+    
+
+}
+    
+}
---
+++ pw3.java
@@ -0,0 +1,12 @@
+class pw3
+{
+  public static void main(String[] args){
+   int a=20;
+   int b=30;
+   int c=a+b;
+   System.out.println("the sum of two number is:"+c);
+
+  }
+  
+    
+}
